490d97938fc0d7e07b99dac01e7c4691315590b8
ianfab <ianfab@users.noreply.github.com>
2018-01-19 23:12:13 +0100

    Generate elephant and hawk promotions as captures

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -88,29 +88,21 @@ namespace {
 
 
   template<GenType Type, Square D>
-  ExtMove* make_promotions(ExtMove* moveList, Square to, Square ksq) {
+  ExtMove* make_promotions(ExtMove* moveList, Square to) {
 
     if (Type == CAPTURES || Type == EVASIONS || Type == NON_EVASIONS)
+    {
         *moveList++ = make<PROMOTION>(to - D, to, QUEEN);
+        *moveList++ = make<PROMOTION>(to - D, to, ELEPHANT);
+        *moveList++ = make<PROMOTION>(to - D, to, HAWK);
+    }
 
     if (Type == QUIETS || Type == EVASIONS || Type == NON_EVASIONS)
     {
-        *moveList++ = make<PROMOTION>(to - D, to, ELEPHANT);
-        *moveList++ = make<PROMOTION>(to - D, to, HAWK);
         *moveList++ = make<PROMOTION>(to - D, to, ROOK);
         *moveList++ = make<PROMOTION>(to - D, to, BISHOP);
         *moveList++ = make<PROMOTION>(to - D, to, KNIGHT);
     }
-
-    // Elephant/Hawk/Knight promotions are the only promotions that can give a direct check
-    // that's not already included in the queen promotion.
-    if (Type == QUIET_CHECKS && (PseudoAttacks[KNIGHT][to] & ksq))
-    {
-        *moveList++ = make<PROMOTION>(to - D, to, ELEPHANT);
-        *moveList++ = make<PROMOTION>(to - D, to, HAWK);
-    }
-    else
-        (void)ksq; // Silence a warning under MSVC
 
     return moveList;
   }
@@ -199,16 +191,14 @@ namespace {
         Bitboard b2 = shift<Left >(pawnsOn7) & enemies;
         Bitboard b3 = shift<Up   >(pawnsOn7) & emptySquares;
 
-        Square ksq = pos.square<KING>(Them);
-
         while (b1)
-            moveList = make_promotions<Type, Right>(moveList, pop_lsb(&b1), ksq);
+            moveList = make_promotions<Type, Right>(moveList, pop_lsb(&b1));
 
         while (b2)
-            moveList = make_promotions<Type, Left >(moveList, pop_lsb(&b2), ksq);
+            moveList = make_promotions<Type, Left >(moveList, pop_lsb(&b2));
 
         while (b3)
-            moveList = make_promotions<Type, Up   >(moveList, pop_lsb(&b3), ksq);
+            moveList = make_promotions<Type, Up   >(moveList, pop_lsb(&b3));
     }
 
     // Standard and en-passant captures
